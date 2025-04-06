707c8d042918b438cb9415808746fb04ff05573a
folaoluwafemi <folaoluwafemi55@gmail.com>
2025-04-06 20:53:27 +0100

    add facebook config

--- lib/src/modules/auth/data/oauth_source.dart
+++ lib/src/modules/auth/data/oauth_source.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-//
-// class OauthSource {
-//   Future<({String idToken, String accessToken})?> _signInWithGoogle() async {
-//     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-//
-//     final googleAuth = await googleUser?.authentication;
-//
-//     final idToken = googleAuth?.idToken;
-//     final accessToken = googleAuth?.accessToken;
-//
-//     if (idToken == null || accessToken == null) return null;
-//
-//     return (idToken: idToken, accessToken: accessToken);
-//   }
-//
-//
-//   Future<void> _signInWithFacebook() async {
-//     final LoginResult loginResult = await FacebookAuth.instance.login();
-//
-//     loginResult.accessToken;
-//   }
-// }
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class OauthSource {
+  Future<({String idToken, String accessToken})?> _signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final googleAuth = await googleUser?.authentication;
+
+    final idToken = googleAuth?.idToken;
+    final accessToken = googleAuth?.accessToken;
+
+    if (idToken == null || accessToken == null) return null;
+
+    return (idToken: idToken, accessToken: accessToken);
+  }
+
+
+  Future<void> _signInWithFacebook() async {
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    loginResult.accessToken;
+  }
+}
